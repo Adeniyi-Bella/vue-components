0e6136dbac13f58c89da9be78b5c9f0f75af4556
Adeniyi-Bella <adeniyibella@gmail.com>
2023-02-11 17:42:18 +0100

    composition api component

---
+++ src/components/composition_api/BookCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="!book.image.includes('undefined') && book.image" class="mb-15" @click="onBookClick(book)">
+    <img :src="book.image" alt="" />
+    <div>{{ book.title }}</div>
+  </div>
+</template>
+<script>
+
+
+export default {
+  props: {
+    book: {
+      type: Object,
+      required: true
+    },
+    onBookClick: {
+      type: Function,
+      required: true
+    }
+  }
+};
+</script>
